ddb0385c7816210fcdd593f486f2891afb0eca10
Nuralla Muzamelov <muzamelov.nuralla@jits-3-19.jits.ipa.dc>
2022-05-29 11:07:35 +0600

    first commit

--- src/App.js
+++
@@ -1,9 +0,0 @@
-// import logo from "./logo.svg";
-import "./App.css";
-import Registration from "./components/registration/registration";
-
-function App() {
-  return <Registration />;
-}
-
-export default App;
